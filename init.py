6cbbfe0b016e892d72aea26747c0a85eb99a824f
nithin-sudarsan <nithinsudarsan740@gmail.com>
2025-06-15 18:11:24 +0530

    gitlite initialization - refined

--- init.py
+++ init.py
@@ -8,12 +8,12 @@ def init_repo(print_msg, new_dir=""):
     #   From def init return that error message instead of None
     if new_dir!="":
         try:
-            os.mkdir(new_dir)
+            os.mkdir(new_dir, 0o777)
             os.chdir(new_dir)
         except FileExistsError:
-            print(f"Directory '{new_dir}' already exists.")
+            return f"Directory '{new_dir}' already exists."
         except Exception as e:
-            print(f"An error occurred: {e}")
+            return f"An error occurred: {e}"
             
     dir_name = '.gitlite'
     try:
@@ -25,43 +25,43 @@ def init_repo(print_msg, new_dir=""):
                 with open("index", "w") as f:
                     pass
             except Exception as e:
-                print(f"Error creating INDEX file: {e}")
-                pass
+                return f"Error creating INDEX file: {e}"
             for c in contents:
                 try:
                     os.makedirs(c, 0o777)
                 except:
-                    print("Unable to create ", c)
+                    return f"Unable to create {c}"
 
             try:
                 with open("refs/HEAD", "w") as f:
                     f.write("ref: refs/heads/main\n")
             except Exception as e:
-                print(f"Error creating HEAD file: {e}")
+                return f"Error creating HEAD file: {e}"
             try:
                 with open("refs/heads/main", "w") as f:
                     pass
             except Exception as e:
-                print(f"Error creating main branch: {e}")
+                return f"Error creating main branch: {e}"
 
         except Exception as e:
-            print(e)
+            return (e)
         if print_msg:
-            print("Initialised an empty gitLite repository")
+            return ("Initialised an empty gitLite repository")
+        return None
 
     except FileExistsError:
         shutil.rmtree(dir_name, ignore_errors=False)
         init_repo(False, "")
-        print(f"Reinitialize gitLite repository in '{os.path.dirname(os.getcwd())}'.")
+        return f"Reinitialize gitLite repository in '{os.path.dirname(os.getcwd())}'."
     except Exception as e:
-        print(f"An error occurred: {e}")
+        return f"An error occurred: {e}"
     
 
 def init(cmd:str, dir = None):
     if dir and dir!="Empty":
-        init_repo(True, dir)
+        err = init_repo(True, dir)
     elif dir and dir=="Empty":
-        init_repo(True, "")
+        err = init_repo(True, "")
     else:
         return throw_error(cmd, invalid= True)
-    return None
+    return err
